de86d8ee0c8b71fdf28458004acb3f36bd63cc48
Illia Bilous <krabbys1111@gmail.com>
2025-08-17 08:37:11 +0300

    Add Terraform configuration for Azure resources and outputs

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+  name     = var.resource_group_name
+  location = var.location
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "examplestorageacc"     # має бути унікальним по всьому Azure!
+  name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
